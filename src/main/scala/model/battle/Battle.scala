63ae3a4f1be9a1b0650bca5b31089cdb35492121
Marco Avagnano <marco.avagnano@studio.unibo.it>
2023-06-23 15:42:52 +0200

    feat: battle logic only for attack choice

--- src/main/scala/model/battle/Battle.scala
+++ src/main/scala/model/battle/Battle.scala
@@ -1,7 +1,9 @@
 package model.battle
 
-import model.entities.{Trainer}
-import model.entities.pokemon.Pokemon
+import model.entities.Trainer
+import model.entities.pokemon.{Move, Pokemon}
+
+import scala.language.postfixOps
 /**
  * Battle models clash between two trainers
  */
@@ -18,21 +20,41 @@ trait Battle:
    */
   def opponent: Trainer
 
-  /**
-   *
-   * @return list player's pokemon
-   */
-  def playerPokemon: List[Pokemon]
-  /**
-   *
-   * @return list opponent's pokemon
-   */
-  def opponentPokemon: List[Pokemon]
 
   /**
    *
    * @return an instance of new [[Fight]]
    */
-  def startFight: Fight
+  def fight(playerMove: Move, aiMove:Move): Boolean
+
+object Battle:
+  def apply(player: Trainer, opponent: Trainer): Battle =
+    BattleImpl(player,opponent)
+  private case class BattleImpl(override val player: Trainer,
+                                override val opponent: Trainer,
+                                ) extends Battle:
+    var playerPokemon:Seq[Pokemon] = player.pokemonTeam
+    var opponentPokemon:Seq[Pokemon] = opponent.pokemonTeam
+
+    override def fight(playerMove: Move, aiMove:Move): Boolean =
+      import util.Utilities.pop
+      (playerPokemon pop, opponentPokemon pop) match
+        case (Some(p1), Some(p2)) =>
+          playerPokemon = p1._2
+          opponentPokemon = p2._2
+          for
+            updatedPokemon <- Fight(p1._1,p2._1,Map(p1._1.id -> playerMove, p2._1.id -> aiMove))
+          do update(p1._1, p2._1, updatedPokemon)
+          false
+
+        case _ => true
+
+    def update(p1:Pokemon, p2:Pokemon,updated: Pokemon): Unit =
+      updated.id match
+        case id: String if id == p1.id => playerPokemon = playerPokemon.updated(0, updated)
+        case _ => opponentPokemon = opponentPokemon.updated(0, updated)
+
+
+
 
 
--- src/main/scala/model/battle/Fight.scala
+++ src/main/scala/model/battle/Fight.scala
@@ -1,51 +1,73 @@
 package model.battle
 
 import model.entities.pokemon.AdditionalEffects.{GainDamage, SkipTurn}
-import model.entities.pokemon.Pokemon
+import model.entities.pokemon.ElementType.Fire
+import model.entities.pokemon.{ParalyzeStatus, Pokemon, Move}
 
 /**
  * [[Fight]] models clash between two [[Pokemon]]
  */
 trait Fight:
   /**
-   * 
+   *
    * @return player's [[Pokemon]]
    */
   def playerPokemon: Pokemon
+
   /**
    *
    * @return opponent's [[Pokemon]]
    */
   def opponentPokemon: Pokemon
-  
-  type Turn
 
   /**
-   * 
+   *
    * @return defeated [[Pokemon]]
    */
-  def endFight: Pokemon
+  def endFight: Boolean
 
-case class FightTurn(pokemonInBattle: Seq[Pokemon]):
-  extension (seq: Seq[Pokemon])
-    def missTurn: Seq[Pokemon] =
-      seq.filter({
-        case pokemon: Pokemon => pokemon.status match
-          case s:SkipTurn => !s.applyStatus(pokemon)
-          case _ => true
-      })
-
-    def applyStatusEffect : Seq[Pokemon] =
-      seq.map(p => p.status match {
-        case s: GainDamage => s.applyStatus(p)
-        case _ => p
-      })
-
-    def bySpeed: Seq[Pokemon] =
-      seq.sortWith(_.speed > _.speed)
-
-  val generate:Seq[(Pokemon,Pokemon)] =
+object Fight:
+  def apply(p1: Pokemon, p2: Pokemon, moves: Map[String, Move]): Seq[Pokemon] =
     for
-      p <- pokemonInBattle.missTurn.applyStatusEffect.bySpeed
-      p2 <- pokemonInBattle if !p.equals(p2)
-    yield (p,p2)
+      turn <- FightTurn(Seq(p1, p2)).generate
+      alive <- withLife(attack(turn, moves(turn._1.id)))
+    yield alive
+
+
+  def attack(t: (Pokemon, Pokemon), move: Move): Pokemon =
+      val computeTotalDamage: (Int, Int, Int) => Int = (power, attack, defense) => (42 * power * (attack / defense)) / 50
+      move.applyStatus(
+        t._2.withHp(
+        t._2.hp - computeTotalDamage(
+          t._1.attack,
+          t._2.defense,
+          move.damage
+        )))
+
+  def withLife(pokemon: Pokemon): Option[Pokemon] =
+    pokemon.hp match
+    case value: Int if value > 0 => Some(pokemon)
+    case _ => None
+
+
+
+  case class FightTurn(pokemonInBattle: Seq[Pokemon]):
+    extension (seq: Seq[Pokemon])
+      def applyStatusEffect: Seq[Pokemon] =
+        seq.filter({
+          case pokemon: Pokemon => pokemon.status match
+            case s: SkipTurn => !s.applyStatus(pokemon)
+            case _ => true
+        }).map(p => p.status match {
+          case s: GainDamage => s.applyStatus(p)
+          case _ => p
+        })
+
+      def bySpeed: Seq[Pokemon] =
+        seq.sortWith(_.speed > _.speed)
+
+    val generate: Seq[(Pokemon, Pokemon)] =
+      for
+        p <- pokemonInBattle.applyStatusEffect.bySpeed
+        p2 <- pokemonInBattle if !p.equals(p2)
+      yield (p, p2)
