d6859b2d9f8fda3836022625dce174fa974ee11b
Simone Fontanella <simone.fontanella@studio.unibo.it>
2023-07-03 15:33:25 +0200

    test: add test for Pokemon class

--- src/test/scala/model/PokemonFactoryTest.scala
+++ src/test/scala/model/PokemonFactoryTest.scala
@@ -10,7 +10,7 @@ class PokemonFactoryTest extends AnyFlatSpec with should.Matchers:
 
   private val bulbasaur = Pokemon("1","Bulbasaur",45,49,49,45,List(),ElementType.Grass)
   "A PokemonFactory" should "create a seq of 4 pokemon" in{
-    assertResult(4)(PokemonFactory.getRandomPokemons(4).length)
+    assertResult(4)(PokemonFactory(4).length)
   }
 
   "A PokemonFactory" should "return Bulbasaur with specified id 1" in{
---
+++ src/test/scala/model/PokemonTest.scala
@@ -0,0 +1,16 @@
+package model
+
+import model.entities.pokemon.{ElementType, Pokemon, PokemonFactory}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class PokemonTest extends AnyFlatSpec with should.Matchers:
+  private val bulbasaur = Pokemon("1","Bulbasaur",45,49,49,45,List(),ElementType.Grass)
+  
+  "A Pokemon" should "not have an hp update below 0" in{
+    assertResult(0)((bulbasaur withHp -1).hp)
+  }
+
+  "A Pokemon" should "not have an hp update that exceed his max life" in{
+    assertResult(45)((bulbasaur withHp 100).hp)
+  }
