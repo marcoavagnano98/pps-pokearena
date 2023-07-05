8ac5fc429dfe198116b4ced2bffd23dfef6d3c93
Marco Avagnano <marco.avagnano@studio.unibo.it>
2023-07-05 19:27:48 +0200

    test: add test for battle engine

--- src/test/scala/model/TestBattle.scala
+++ src/test/scala/model/TestBattle.scala
@@ -1,19 +1,37 @@
 package model
 
+import model.entities.Player
 import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
-//import model.battle.FightTurn
 import model.entities.pokemon.*
 import model.entities.pokemon.ElementType.*
+import model.entities.Trainer
+import model.battle.*
 
 import scala.language.postfixOps
 
-class TestBattle extends AnyFlatSpec with should.Matchers
-  /*val p1: Pokemon = Pokemon("a", hp = 100, attack = 100, defense = 100, speed = 100, moves = List(), elementType = Fire) withStatus ParalyzeStatus()
-  val p2: Pokemon = Pokemon("b", hp = 100, attack = 100, defense = 100, speed = 101, moves = List(), elementType = Fire)
-  val turn: FightTurn = FightTurn(Seq(p1, p2))
-  "A FightTurn" should "has a list of a tuple where the opponent pokemon is first and player pokemon is last" in{
-    assertResult(turn.generate)(Seq((p2,p1)))
-  }*/
+class TestBattle extends AnyFlatSpec with should.Matchers:
+
+  val bulbasaur: Pokemon = PokemonFactory.getPokemonById("1").get
+  val charmender: Pokemon = PokemonFactory.getPokemonById("4").get
+  val action: Move = Move(20, 10, "azione", Normal, None)
+  val lethalMove: Move = Move(9999, 10, "azione", Normal, None)
+  val player: Player = Player(0,0,10,10,"",PokemonFactory(3))
+  val opponent: Trainer = Trainer(0,0,10,10,"",PokemonFactory(3))
+  val slowestBt: BattleUnit = BattleUnit(bulbasaur, player, BattleOption.Attack(action))
+  val fastestBt: BattleUnit = BattleUnit(charmender, opponent, BattleOption.Attack(action))
+
+  "A BattleEngine " should " return the fastest Pokemon in battle first" in{
+   BattleEngine(slowestBt, fastestBt).head.pokemon.id shouldBe charmender.id
+  }
+  "A BattleEngine " should " return damaged battle units after attacks" in{
+    BattleEngine(slowestBt, fastestBt) shouldBe Seq(BattleEngine.unitAfterAttack(slowestBt, fastestBt,action), BattleEngine.unitAfterAttack(fastestBt, slowestBt, action))
+  }
+  val lethalBt: BattleUnit = BattleUnit(bulbasaur, player, BattleOption.Attack(lethalMove))
+
+  "A BattleEngine " should " return only alive battle units" in{
+    BattleEngine(lethalBt, fastestBt) shouldBe Seq(BattleEngine.unitAfterAttack(fastestBt,lethalBt ,action))
+  }
+
 
