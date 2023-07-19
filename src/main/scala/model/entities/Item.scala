b4bf4fcde4cab6ca4fdace6075c82c9fe28cdf4e
simonefontanella <fonta9879@gmail.com>
2023-07-19 13:17:35 +0200

    docs: Add docs to item trait

--- src/main/scala/model/entities/Item.scala
+++ src/main/scala/model/entities/Item.scala
@@ -5,17 +5,24 @@ import model.entities.pokemon.Pokemon
 import model.entities.pokemon.StatusEffects.ChangeHpEffect
 
 trait Item extends VisibleEntity :
+  /**
+   * @return the name of [[Item]]
+   */
   def name: String
 
+  /**
+   * @param pokemon The [[Pokemon]] to which the [[Item]] was used on.
+   * @return The new [[Pokemon]] after the [[Item]] was applied.
+   */
   def use(p: Pokemon): Pokemon
 
-private class Potion(override val id: String = "Item_01", override val position: Position, override val name: String = "Potion") extends Item with ChangeHpEffect :
+private case class Potion(override val id: String = "Item_01", override val position: Position, override val name: String = "Potion") extends Item with ChangeHpEffect :
   override def hpToChange: Int = 20
 
   override def use(p: Pokemon): Pokemon =
     applyChangeStat(p)
 
-private class SuperPotion(override val id: String = "Item_02", override val position: Position, override val name: String = "Potion") extends Item with ChangeHpEffect :
+private case class SuperPotion(override val id: String = "Item_02", override val position: Position, override val name: String = "Potion") extends Item with ChangeHpEffect :
   override def hpToChange: Int = 50
 
   override def use(p: Pokemon): Pokemon =
@@ -25,13 +32,13 @@ enum ItemId:
   case Potion, SuperPotion
 
 object ItemFactory:
-  def apply(itemId: ItemId, position: Position) : Item = itemId match
-    case ItemId.Potion => Potion(position=position)
-    case ItemId.SuperPotion => SuperPotion(position=position)
+  def apply(itemId: ItemId, pos: Position = Position(0,0)) : Item = itemId match
+    case ItemId.Potion => Potion(position=pos)
+    case ItemId.SuperPotion => SuperPotion(position=pos)
 
-  def getRandomItem(position: Position) : Item =
+  def getRandomItem(pos: Position) : Item =
     import scala.util.Random
     Random.nextInt(ItemId.values.length) match
-      case 1 => apply(ItemId.SuperPotion,position)
-      case _ => apply(ItemId.Potion,position)
+      case 1 => apply(ItemId.SuperPotion,pos)
+      case _ => apply(ItemId.Potion,pos)
 
