52a8557ae70ead42dea238343d57f595edca9c90
Marco Avagnano <marco.avagnano@studio.unibo.it>
2023-07-25 13:13:01 +0200

    chore: set fixed windows size

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -5,11 +5,12 @@ object Launcher:
   @main
   def main(): Unit =
     val config = Lwjgl3ApplicationConfiguration()
+    val size =(1000, 1000)
     config.setTitle("PokeArena")
     config.useVsync(true) // Limits FPS to the refresh rate of the currently active monitor.
     config.setWindowIcon("assets/icon/pokearena.png")
-    config.setWindowSizeLimits(700, 700, 9999, 9999);
+    config.setWindowSizeLimits(size._1, size._2, size._1, size._2);
     config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode.refreshRate)
-    config.setResizable(System.getProperty("os.arch") != "aarch64")
-    config.setWindowedMode(1000, 1000)
+   // config.setResizable(System.getProperty("os.arch") != "aarch64")
+   // config.setWindowedMode(size._1, size._2)
     Lwjgl3Application(PokeArena, config)
