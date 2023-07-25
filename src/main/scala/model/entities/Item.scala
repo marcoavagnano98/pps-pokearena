1914eb28cc8ca536a56f886b8f587a2c6b134162
Simone Fontanella <simone.fontanella@studio.unibo.it>
2023-07-25 17:02:35 +0200

    refactor: SuperPotion extends from Potion

--- src/main/scala/model/entities/Item.scala
+++ src/main/scala/model/entities/Item.scala
@@ -16,30 +16,26 @@ trait Item extends VisibleEntity :
    */
   def use(pokemon: Pokemon): Pokemon
 
-private case class Potion(override val id: String = "Item_01", override val position: Position, override val name: String = "Potion") extends Item with ChangeHpEffect :
+private class Potion(override val id: String = "Item_01", override val position: Position, override val name: String = "Potion") extends Item with ChangeHpEffect :
   override def hpToChange: Int = 20
 
   override def use(p: Pokemon): Pokemon =
     applyChangeStat(p)
 
-private case class SuperPotion(override val id: String = "Item_02", override val position: Position, override val name: String = "Potion") extends Item with ChangeHpEffect :
+private class SuperPotion(override val position: Position) extends Potion("Item_02", position, "Super Potion") :
   override def hpToChange: Int = 50
-
-  override def use(p: Pokemon): Pokemon =
-    applyChangeStat(p)
-    
 
 enum ItemId:
   case Potion, SuperPotion
 
 object ItemFactory:
-  def apply(itemId: ItemId, pos: Position = Position(0,0)) : Item = itemId match
-    case ItemId.Potion => Potion(position=pos)
-    case ItemId.SuperPotion => SuperPotion(position=pos)
+  def apply(itemId: ItemId, pos: Position = Position(0, 0)): Item = itemId match
+    case ItemId.Potion => Potion(position = pos)
+    case ItemId.SuperPotion => SuperPotion(position = pos)
 
-  def getRandomItem(pos: Position) : Item =
+  def getRandomItem(pos: Position): Item =
     import scala.util.Random
     Random.nextInt(ItemId.values.length) match
-      case 1 => apply(ItemId.SuperPotion,pos)
-      case _ => apply(ItemId.Potion,pos)
+      case 1 => apply(ItemId.SuperPotion, pos)
+      case _ => apply(ItemId.Potion, pos)
 
