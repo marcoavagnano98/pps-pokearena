ec8acc144593daf4e6d8f5d41cfa527141dc1591
Simone Fontanella <simone.fontanella@studio.unibo.it>
2023-08-02 17:13:31 +0200

    feat: add new item Antidote

--- src/test/scala/model/ItemTest.scala
+++ src/test/scala/model/ItemTest.scala
@@ -8,11 +8,13 @@ import model.entities.{ItemFactory, Potion}
 import model.entities.pokemon.ElementType
 import model.entities.World.Position
 import model.entities.ItemType
+import model.entities.pokemon.AllPokemonStatus.{BurnStatus, HealthyStatus}
 class ItemTest extends AnyFlatSpec with should.Matchers:
 
   private var bulbasaur = Pokemon("1","Bulbasaur",50,49,49,45,List(),ElementType.Grass)
   private val potion = ItemFactory(ItemType.Potion)
   private val superPotion = ItemFactory(ItemType.SuperPotion)
+  private val antidote = ItemFactory(ItemType.Antidote)
 
   "A potion" should "increase the amount of hp of pokemon by 30" in {
     bulbasaur = bulbasaur withHp 30
@@ -24,5 +26,11 @@ class ItemTest extends AnyFlatSpec with should.Matchers:
     bulbasaur = bulbasaur withHp 0
     bulbasaur = superPotion use bulbasaur
     assertResult(50)(bulbasaur.hp)
+  }
+
+  "A antidote" should "remove the current status of pokemon and change it into HealthyStatus" in {
+    bulbasaur = bulbasaur withStatus BurnStatus()
+    bulbasaur = antidote use bulbasaur
+    assertResult(HealthyStatus())(bulbasaur.status)
   }
 
